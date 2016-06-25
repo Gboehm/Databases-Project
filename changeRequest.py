0ac01b7eb1029a59f9a999ffb4a3a3c005936251
eabidor <abidor.e@husky.neu.edu>
2016-06-25 12:34:42 -0400

    changereq update

--- changeRequest.py
+++ changeRequest.py
@@ -1,10 +1,10 @@
 from mysql.connector import cursor
 
-def changeRequest(cursor):
+def changerequest(cursor, id):
     while 1:
         street = input("Please provide the name of the street for the change request: \n")
 
-        stmt = ("SELECT s.streetname, f.description, f.startaddress, f.endaddress, f.featureid "
+        stmt = ("SELECT s.streetname, f.description, f.startaddress, f.endaddress, f.featureid, s.streetid "
                 "FROM streets s "
                 "INNER JOIN features f "
                 "ON f.streetid = s.streetid "
@@ -14,6 +14,7 @@ def changeRequest(cursor):
         result = cursor.fetchone()
 
         if result:
+            streetid = result[5]
             print("feature id: ", result[4])
             if result[2]:
                 if result[3]:
@@ -34,11 +35,57 @@ def changeRequest(cursor):
         else:
             print(result[0], "has a", result[1])
 
-    While 1:
+    while 1:
         type = input("Would you like to make a new feature, update an existing feature, or delete a feature?\n",
                      "input 'new', 'update', or 'delete")
         if type.lower() == 'new':
+            start = input('If there is a start address, input it. Otherwise press enter')
+            end = input('If there is an end address, input it. Otherwise press enter')
+            desc = input('Please input the bike feature description:')
+            if start.isdigit():
+                if end.isdigit():
+                    new = ("INSERT INTO changerequest "
+                           "(userid, changetype, streetid, startaddress, endaddress, description) "
+                           "VALUES (%s, %s, %s, %s, %s, %s)")
+                    cursor.execute(new, (id,"new",streetid,start,end, desc))
+                else:
+                    new = ("INSERT INTO changerequest "
+                           "(userid, changetype, streetid, startaddress,description) "
+                           "VALUES (%s, %s, %s, %s, %s)")
+                    cursor.execute(new, (id, "new", streetid, start, desc))
+            else:
+                new = ("INSERT INTO changerequest "
+                       "(userid, changetype, streetid, startaddress, endaddress, description) "
+                       "VALUES (%s, %s, %s, %s)")
+                cursor.execute(new, (id, "new", streetid, desc))
+            break
 
         if type.lower() == 'update':
-
+            featid = "Please input the feature id from the above list you would like to update:"
+            start = input('If changing/adding start address, input it. Otherwise press enter')
+            end = input('If changing/adding end address, input it. Otherwise press enter')
+            desc = input('Please input updated bike feature description:')
+            if start.isdigit():
+                if end.isdigit():
+                    new = ("INSERT INTO changerequest "
+                           "(userid, featureid, changetype, streetid, startaddress, endaddress, description) "
+                           "VALUES (%s, %s, %s, %s, %s, %s, %s)")
+                    cursor.execute(new, (id,featid,"update",streetid,start,end, desc))
+                else:
+                    new = ("INSERT INTO changerequest "
+                           "(userid, featureid, changetype, streetid, startaddress,description) "
+                           "VALUES (%s, %s, %s, %s, %s, %s)")
+                    cursor.execute(new, (id, featid, "update", streetid, start, desc))
+            else:
+                new = ("INSERT INTO changerequest "
+                       "(userid, featureid, changetype, streetid, startaddress, endaddress, description) "
+                       "VALUES (%s, %s, %s, %s, %s)")
+                cursor.execute(new, (id, featid, "update", streetid, desc))
+            break
         if type.lower() == 'delete':
+            featid = "Please input the feature id from the above list you would like to delete:"
+            delete = ("INSERT INTO changerequest "
+                   "(userid, featureid, changetype, streetid) "
+                   "VALUES (%s, %s, %s)")
+            cursor.execute(delete, (id, featid, "delete", streetid))
+            break
