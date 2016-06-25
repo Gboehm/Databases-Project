aa72c2c7a2451bb4be2f9ce430a37611940a5f86
eabidor <abidor.e@husky.neu.edu>
2016-06-25 14:52:10 -0400

    also delete

--- deleterequest.py
+++ deleterequest.py
@@ -3,13 +3,31 @@ from mysql.connector import cursor
 
 def deleterequest(cursor, id, placeid, which):
     if which == 0:
-        featid = "Please input the feature id from the above list you would like to delete:"
+        while 1:
+            featid = "Please input the feature id from the above list you would like to update:"
+            isafeat = 0
+            for result in results:
+                if result[4] == featid:
+                    isafeat = 1
+            if isafeat:
+                break
+            else:
+                print("Not a valid feature id from the results. Try again")
         delete = ("INSERT INTO changerequest "
                   "(userid, featureid, changetype, streetid) "
                   "VALUES (%s, %s, %s, %s)")
         cursor.execute(delete, (id, featid, "delete", placeid))
     elif which == 1:
-        featid = "Please input the feature id from the above list you would like to delete:"
+        while 1:
+            featid = "Please input the feature id from the above list you would like to update:"
+            isafeat = 0
+            for result in results:
+                if result[2] == featid:
+                    isafeat = 1
+            if isafeat:
+                break
+            else:
+                print("Not a valid feature id from the results. Try again")
         delete = ("INSERT INTO changerequest "
                   "(userid, featureid, changetype, intersectionid) "
                   "VALUES (%s, %s, %s, %s)")
