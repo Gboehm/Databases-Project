1a946d8bcc7e9a691047723a7b0ebdc359c812fd
Gunnar Boehm <gunnarman@msn.com>
2016-06-25 12:17:16 -0400

    Added incidents to intersections

--- View.py
+++ View.py
@@ -6,7 +6,7 @@ def View(cursor):
         type = input("View a Street or an Intersection?\n")
 
         if type.lower() == 'street':
-            operation = input("View incidents or features?\n")
+            operation = input("View Incidents or Features?\n")
 
             if operation.lower() == "features":
                 while 1:
@@ -73,6 +73,10 @@ def View(cursor):
                         if result[3]:
                             print("Description:", result[3], '\n')
                         break
+                    else:
+                        print("No incidents found on", street)
+            else:
+                print("Invalid input")
 
                 for result in cursor:
                     print("Street:", result[0])
@@ -92,47 +96,103 @@ def View(cursor):
 
 
         elif type.lower() == 'intersection':
-            while 1:
-                street1 = input("Please input a cross street name.\n")
-                street2 = input("Please input the other cross street name.\n")
+            operation = input("View Incidents or Features?\n")
 
-                stmt = ("SELECT DISTINCT f.description "
-                    "FROM features f "
-                    "INNER JOIN intersections i "
-                    "ON f.intersectionid = i.intersectionid "
-                    "INNER JOIN streets s "
-                    "ON i.street1 = s.streetid "
-                    "OR i.street2 = s.streetid "
-                    "WHERE s.streetname = %s AND "
-                    "i.intersectionid IN "
-                    "(SELECT i.intersectionid "
-                    " FROM intersections i "
-                    " INNER JOIN streets s "
-                    " ON i.street1 = s.streetid "
-                    " OR i.street2 = s.streetid "
-                    " WHERE s.streetname = %s) "
-                    )
+            if operation.lower() == 'features':
 
-                cursor.execute(stmt, (street1, street2))
-                result = cursor.fetchone()
+                while 1:
+                    street1 = input("Please input a cross street name.\n")
+                    street2 = input("Please input the other cross street name.\n")
 
-                if result:
-                    print("The intersection of", street1, "and", street2, "has the following:")
+                    stmt = ("SELECT DISTINCT f.description "
+                            "FROM features f "
+                            "INNER JOIN intersections i "
+                            "ON f.intersectionid = i.intersectionid "
+                            "INNER JOIN streets s "
+                            "ON i.street1 = s.streetid "
+                            "OR i.street2 = s.streetid "
+                            "WHERE s.streetname = %s AND "
+                            "i.intersectionid IN "
+                            "(SELECT i.intersectionid "
+                            " FROM intersections i "
+                            " INNER JOIN streets s "
+                            " ON i.street1 = s.streetid "
+                            " OR i.street2 = s.streetid "
+                            " WHERE s.streetname = %s) "
+                            )
+
+                    cursor.execute(stmt, (street1, street2))
+                    result = cursor.fetchone()
+
+                    if result:
+                        print("The intersection of", street1, "and", street2, "has the following:")
+                        print(result[0])
+                        break
+                    else:
+                        print("No features found on the intersection of", street1, "and", street2)
+
+                        while 1:
+                            op = input("Search again? Yes or No\n")
+
+                            if op.lower() == 'no' or op.lower() == 'yes':
+                                break
+                    if op and op.lower() == 'no':
+                        break
+
+                for result in cursor:
                     print(result[0])
-                    break
-                else:
-                    print("No features found on the intersection of", street1, "and", street2)
+            elif operation.lower() == 'incidents':
+                while 1:
+                    street1 = input("Please input a cross street name.\n")
+                    street2 = input("Please input the other cross street name.\n")
 
-                    while 1:
-                        op = input("Search again? Yes or No\n")
+                    stmt = ("SELECT DISTINCT inc.address, inc.severity, inc.description "
+                            "FROM incidents inc "
+                            "INNER JOIN intersections i "
+                            "ON inc.intersectionid = i.intersectionid "
+                            "INNER JOIN streets s "
+                            "ON i.street1 = s.streetid "
+                            "OR i.street2 = s.streetid "
+                            "WHERE s.streetname = %s AND "
+                            "i.intersectionid IN "
+                            "(SELECT i.intersectionid "
+                            " FROM intersections i "
+                            " INNER JOIN streets s "
+                            " ON i.street1 = s.streetid "
+                            " OR i.street2 = s.streetid "
+                            " WHERE s.streetname = %s) "
+                            )
+                    cursor.execute(stmt, (street1, street2))
+                    result = cursor.fetchone()
 
-                        if op.lower() == 'no' or op.lower() == 'yes':
-                            break
-                if op and op.lower() == 'no':
-                    break
+                    if result:
+                        if result[0]:
+                            print("Address:", result[0])
+                        if result[1]:
+                            print("Severity:", result[1])
+                        if result[2]:
+                            print("Description:", result[2], '\n')
+                        break
+                    else:
+                        print("No incidents found on the intersection of", street1, "and", street2)
 
-        for result in cursor:
-            print(result[0])
+                        while 1:
+                            op = input("Search again? Yes or No\n")
+
+                            if op.lower() == 'no' or op.lower() == 'yes':
+                                break
+                    if op and op.lower() == 'no':
+                        break
+
+                for result in cursor:
+                    if result[0]:
+                        print("Address:", result[0])
+                    if result[1]:
+                        print("Severity:", result[1])
+                    if result[2]:
+                        print("Description:", result[2], '\n')
+            else:
+                print('Invalid input')
 
         while 1:
             op = input("View again? Yes or No?\n")
